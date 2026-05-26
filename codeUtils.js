1a8c4bee0e71ec32570e64aa542b2562e37e6da2
Matias <68409669+mCodeCode@users.noreply.github.com>
2026-05-25 21:13:21 -0300

    Update codeUtils.js

--- codeUtils.js
+++ codeUtils.js
@@ -1,15 +1,4 @@
-//global vars , and draw helper functions
-const colorList = {
-  red: "#ff0000",
-  green: "#00ff15",
-  blue: "#0004ff",
-  yellow: "#f0d826",
-  neonOrange: "#e34c32",
-  purple: "#4f04bf",
-  greenBlue: "#04b6bf",
-  white: "#ffffff",
-  black: "#000000",
-};
+//global vars , and helper functions
 
 //----------------------------------------------------
 //----------------------------------------------------
@@ -33,17 +22,3 @@ function getRandomPosition(entityMinPlacement, entityMaxPlacementX, entityMaxPla
   const z = Math.random() * (entityMaxPlacementZ - -entityMinPlacement) + -entityMinPlacement;
   return { x, y, z };
 }
-
-//----------------------------------------------------
-//----------------------------------------------------
-function convertDegreesToRadians(degrees) {
-  return degrees * (Math.PI / 180);
-}
-//----------------------------------------------------
-//----------------------------------------------------
-function removeDecimals(fromNumber) {
-  return Number.parseFloat(fromNumber).toFixed(7);
-}
-
-//----------------------------------------------------
-//----------------------------------------------------
